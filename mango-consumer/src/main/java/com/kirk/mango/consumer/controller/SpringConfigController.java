d89861637cf192e550e54f15a7035884526237f4
liuwei01437 <liuwei01437@i2finance.net>
2019-12-06 16:56:33 +0800

    mango

--- mango-consumer/src/main/java/com/kirk/mango/consumer/controller/SpringConfigController.java
+++ mango-consumer/src/main/java/com/kirk/mango/consumer/controller/SpringConfigController.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
 @RestController
 class SpringConfigController {
     
